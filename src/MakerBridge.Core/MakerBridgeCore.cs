d0a7e66f4af01f72f9dc7c505c6ea26f5ad2db6d
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2019-09-07 23:56:42 +0300

    MakerBridgeCore, temp card path change

---
+++ src/MakerBridge.Core/MakerBridgeCore.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Harmony;
+using BepInEx.Logging;
+using System.IO;
+using UnityEngine;
+
+namespace KeelPlugins
+{
+    public class MakerBridgeCore : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.makerbridge";
+        public const string PluginName = "MakerBridge";
+        internal static new ManualLogSource Logger;
+
+        internal static string MakerCardPath;
+        internal static string OtherCardPath;
+        protected static GameObject bepinex;
+
+        internal static ConfigWrapper<KeyboardShortcut> SendChara { get; set; }
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            bepinex = gameObject;
+
+            SendChara = Config.GetSetting("Keyboard Shortcuts", "Send character", new KeyboardShortcut(KeyCode.B));
+            
+            MakerCardPath = Path.Combine(Paths.ConfigPath, "makerbridge1.png");
+            OtherCardPath = Path.Combine(Paths.ConfigPath, "makerbridge2.png");
+
+            HarmonyWrapper.PatchAll();
+        }
+    }
+}
