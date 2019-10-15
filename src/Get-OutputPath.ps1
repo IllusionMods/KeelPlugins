97277b12c0662319e7f212b07e90af0f51e8fd90
Keelhauled <keelhauledhs@gmail.com>
2019-10-15 18:12:52 +0300

    Powershell build script

---
+++ src/Get-OutputPath.ps1
@@ -0,0 +1,11 @@
+﻿param (
+	[Parameter(Mandatory=$true)][string]$config
+)
+
+try {
+	[xml]$XmlDocument = Get-Content -Path ..\BuildSettings.AISyoujyo.props
+    $XmlDocument.Project.PropertyGroup.Where({$_.condition.contains($config)}).OutputPath
+}
+catch {
+	exit 1
+}
