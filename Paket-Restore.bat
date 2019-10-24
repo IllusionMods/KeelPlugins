3c181f04a59108a79ba68533b2a7f4251e95f6ff
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2019-10-25 01:20:55 +0300

    Add paket.exe again

--- Paket-Restore.bat
+++ Paket-Restore.bat
@@ -1,2 +1,1 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex (Invoke-WebRequest 'https://gist.githubusercontent.com/Keelhauled/1faf77823bda812ff4b1e9113bed783a/raw/42ece4a0e8924695a8476674d6a5c0a39d8d4dd9/Paket-Download.ps1')"
 .paket\paket.exe restore
