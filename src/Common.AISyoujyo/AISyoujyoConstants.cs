d91d5c135dab44e2988a42ea4c0a0d017cc1849a
Mantas-2155X <syenergyy@gmail.com>
2019-11-03 23:04:16 +0200

    TitleShortcuts Support for AISyoujyo Common file added for AISyoujyo

---
+++ src/Common.AISyoujyo/AISyoujyoConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace KeelPlugins
+{
+    internal static class AISyoujyoConstants
+    {
+        public const string MainGameProcessName = "AI-Syoujyo";
+        public const string StudioProcessName = "StudioNEOV2";
+    }
+}
