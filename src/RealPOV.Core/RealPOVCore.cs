2bdfda6c3b9639e2f4d38631303ea2780a61d856
Keelhauled <keelhauledhs@gmail.com>
2019-12-04 18:23:42 +0200

    Restructure pov plugins

---
+++ src/RealPOV.Core/RealPOVCore.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace KeelPlugins
+{
+    public abstract class RealPOVCore : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.realpov";
+        public const string PluginName = "RealPOV";
+        public const string Version = "1.0.1." + BuildNumber.Version;
+
+        internal const string SECTION_GENERAL = "General";
+        internal const string SECTION_HOTKEYS = "Keyboard shortcuts";
+
+        internal static new ManualLogSource Logger;
+
+        protected virtual void Awake()
+        {
+            Logger = base.Logger;
+        }
+    }
+}
