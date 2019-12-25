16e24229f0abf835bf385a8d68affbe532146756
Keelhauled <keelhauledhs@gmail.com>
2019-12-25 05:21:28 +0200

    Testing project

---
+++ src/Testing.AISyoujyo/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Harmony;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace KeelPlugins
+{
+    [BepInPlugin("keelhauled.testing", "TESTINGPLUGIN", "1.0.0")]
+    public class Testing : BaseUnityPlugin
+    {
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            harmony = HarmonyWrapper.PatchAll(GetType());
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchAll();
+        }
+    }
+}
