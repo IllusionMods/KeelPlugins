ea3404bb86f9509d60c665fe235bf43b3cbceada
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-06-11 12:10:13 +0300

    CameraFrameMask plugin

---
+++ src/CameraFrameMask.Koikatu/CameraFrameMask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using BepInEx.Harmony;
+using HarmonyLib;
+using UnityEngine;
+
+namespace KeelPlugins
+{
+    [BepInProcess(KoikatuConstants.MainGameProcessName)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class CameraFrameMask : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.cameraframemask";
+        public const string PluginName = "CameraFrameMask";
+        public const string Version = "1.0.0" + BuildNumber.Version;
+
+        private static Harmony harmony;
+        private static MaskComponent maskComponent;
+
+        public void Awake()
+        {
+            harmony = HarmonyWrapper.PatchAll();
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(CustomScene), "Start")]
+        public static void MakerEntrypoint()
+        {
+            maskComponent = Camera.main.GetOrAddComponent<MaskComponent>();
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(CustomScene), "OnDestroy")]
+        public static void MakerEnd()
+        {
+            maskComponent = null;
+            Destroy(Camera.main.GetComponent<MaskComponent>());
+        }
+
+        public void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.B))
+                maskComponent?.MaskFrames(60);
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchAll();
+        }
+    }
+}
