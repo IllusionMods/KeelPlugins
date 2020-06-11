5790eb0ee9e4d552b58d10317592c78e850ec9c9
Keelhauled <keelhauledhs@gmail.com>
2020-06-11 12:10:13 +0300

    CameraFrameMask plugin

---
+++ src/CameraFrameMask.Koikatu/MaskComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace KeelPlugins
+{
+    public class MaskComponent : MonoBehaviour
+    {
+        private int count = 0;
+        private RenderTexture displayFrame = null;
+
+        public void MaskFrames(int count)
+        {
+            if(this.count < count)
+                this.count = count;
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if(count > 0)
+            {
+                if(displayFrame == null)
+                    displayFrame = src;
+
+                Graphics.Blit(displayFrame, dest);
+                count--;
+            }
+            else
+            {
+                if(displayFrame != null)
+                    displayFrame = null;
+
+                Graphics.Blit(src, dest);
+            }
+        }
+    }
+}
