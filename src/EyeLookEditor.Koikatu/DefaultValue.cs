6a5e5a572c75bfb545a1c464a3777630c0a72654
Keelhauled <keelhauledhs@gmail.com>
2020-06-14 21:10:31 +0300

    EyeLookEditor

---
+++ src/EyeLookEditor.Koikatu/DefaultValue.cs
@@ -0,0 +1,18 @@
+﻿namespace KeelPlugins
+{
+    public static class DefaultValue
+    {
+        public const float ThresholdAngleDifference = 0f;
+        public const float BendingMultiplier = 0.4f;
+        public const float MaxAngleDifference = 10f;
+        public const float UpBendingAngle = -30f;
+        public const float DownBendingAngle = 10f;
+        public const float MinBendingAngle = -36f;
+        public const float MaxBendingAngle = 223f;
+        public const float LeapSpeed = 38f;
+        public const float ForntTagDis = 50f;
+        public const float NearDis = 2f;
+        public const float HAngleLimit = 110f;
+        public const float VAngleLimit = 80f;
+    }
+}
