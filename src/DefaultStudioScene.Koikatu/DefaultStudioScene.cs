e7bfc73cfe15faef8e7dd76de27c485ca938a5a7
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-07-04 16:44:11 +0300

    Plugin DefaultStudioScene

---
+++ src/DefaultStudioScene.Koikatu/DefaultStudioScene.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+
+namespace KeelPlugins
+{
+    [BepInPlugin(GUID, "DefaultStudioScene", Version)]
+    public class DefaultStudioScene : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.defaultstudioscene";
+        public const string Version = "1.0.0." + BuildNumber.Version;
+
+        private static ConfigEntry<string> DefaultScenePath { get; set; }
+
+        public void Awake()
+        {
+            DefaultScenePath = Config.Bind("General", "DefaultScenePath", "");
+            Harmony.CreateAndPatchAll(GetType());
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.Init))]
+        private static void LoadScene()
+        {
+            if(!string.IsNullOrEmpty(DefaultScenePath.Value) && File.Exists(DefaultScenePath.Value))
+                Studio.Studio.Instance.StartCoroutine(Studio.Studio.Instance.LoadSceneCoroutine(DefaultScenePath.Value));
+        }
+    }
+}
