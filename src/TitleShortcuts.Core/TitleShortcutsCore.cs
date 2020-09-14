91d16df4f75861f6f021b8f61383df0b237c3b99
Keelhauled <keelhauledhs@gmail.com>
2020-09-14 23:14:18 +0300

    Remove Autostart option

--- src/TitleShortcuts.Core/TitleShortcutsCore.cs
+++ src/TitleShortcuts.Core/TitleShortcutsCore.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using System;
 using System.Linq;
 
@@ -11,27 +12,30 @@ namespace KeelPlugins
         public const string Version = "1.1.2." + BuildNumber.Version;
 
         protected const string SECTION_HOTKEYS = "Keyboard shortcuts";
-        protected const string SECTION_GENERAL = "General";
-
-        protected const string DESCRIPTION_AUTOSTART = "Choose which mode to start automatically when launching the game.\n" +
-                                                       "Hold esc or F1 during startup to cancel automatic behaviour or hold another shortcut to use that instead.";
 
         protected virtual string[] PossibleArguments { get; }
 
-        protected bool firstLaunch = true;
+        protected static TitleShortcutsCore plugin;
+        protected static new ManualLogSource Logger;
 
-        private string startupArgument;
-        protected string StartupArgument
+        protected virtual void Awake()
+        {
+            plugin = this;
+            Logger = base.Logger;
+        }
+
+        private static string startupArgument;
+        protected static string StartupArgument
         {
             get
             {
                 if(startupArgument == null)
                 {
-                    if(PossibleArguments != null)
+                    if(plugin.PossibleArguments != null)
                     {
                         var args = Environment.GetCommandLineArgs();
                         if(args != null && args.Length > 0)
-                            startupArgument = args.Select(x => x.Trim().ToLower()).FirstOrDefault(x => PossibleArguments.Contains(x));
+                            startupArgument = args.Select(x => x.Trim().ToLower()).FirstOrDefault(x => plugin.PossibleArguments.Contains(x));
                     }
 
                     if(startupArgument == null)
