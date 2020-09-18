0645d2494dd8e8d020927eecf147e7a1d4f8f3c8
Keelhauled <keelhauledhs@gmail.com>
2020-09-18 22:43:13 +0300

    Add ContextMenu plugin

---
+++ src/ContextMenu/ContextMenu.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeelPlugins
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class ContextMenu : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.cameraframemask";
+        public const string PluginName = "CameraFrameMask";
+        public const string Version = "1.0.0." + BuildNumber.Version;
+    }
+}
