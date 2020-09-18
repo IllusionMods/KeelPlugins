468ab72ec84f001af8c49b17ae658761a93caf71
Keelhauled <keelhauledhs@gmail.com>
2020-09-18 22:43:00 +0300

    Finish UnityLogFilter

--- src/UnityLogFilter/Properties/AssemblyInfo.cs
+++ src/UnityLogFilter/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BepinexUnityLogFilter")]
+[assembly: AssemblyTitle("UnityLogFilter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BepinexUnityLogFilter")]
+[assembly: AssemblyProduct("UnityLogFilter")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(KeelPlugins.UnityLogFilter.Version)]
