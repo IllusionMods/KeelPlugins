b7a458bb90ec822e85a7f21b5732dbd416ffdac5
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-10-24 01:27:43 +0300

    UnityLogFilter logging

--- src/UnityLogFilter/UnityLogFilter.cs
+++ src/UnityLogFilter/UnityLogFilter.cs
@@ -12,31 +12,40 @@ namespace KeelPlugins
 {
     public class UnityLogFilter
     {
-        public const string Version = "1.0.0." + BuildNumber.Version;
+        public const string Version = "1.0.1." + BuildNumber.Version;
 
         public static IEnumerable<string> TargetDLLs { get; } = new string[0];
         public static void Patch(AssemblyDefinition ass) { }
 
-        private static Harmony harmony;
-        private static ManualLogSource logger;
-        private static IEnumerable<Regex> filters = new Regex[0];
+        private static Harmony Harmony;
+        private static ManualLogSource Logger;
+
+        private static List<Regex> filters = new List<Regex>();
         private static string FilterFile = Path.Combine(BepInEx.Paths.ConfigPath, "UnityLogFilter.txt");
 
         public static void Finish()
         {
-            harmony = new Harmony(nameof(UnityLogFilter));
-            logger = Logger.CreateLogSource(nameof(UnityLogFilter));
+            Harmony = new Harmony(nameof(UnityLogFilter));
+            Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(UnityLogFilter));
 
             if(File.Exists(FilterFile))
-                filters = File.ReadAllLines(FilterFile).Where(VerifyRegex).Select(x => new Regex(x));
+            {
+                filters = File.ReadAllLines(FilterFile).Where(VerifyRegex).Select(x => new Regex(x)).ToList();
+                Logger.LogInfo($"Loaded {filters.Count} filter{(filters.Count == 1 ? "" : "")}");
+            }
+            else
+            {
+                File.Create(FilterFile);
+                Logger.LogInfo($"{FilterFile} created, add regular expressions to it.");
+            }
 
-            harmony.Patch(AccessTools.Method(typeof(Chainloader), nameof(Chainloader.Initialize)),
-                          postfix: new HarmonyMethod(AccessTools.Method(typeof(UnityLogFilter), nameof(ChainloaderHook))));
+            Harmony.Patch(AccessTools.Method(typeof(Chainloader), nameof(Chainloader.Initialize)),
+                              postfix: new HarmonyMethod(AccessTools.Method(typeof(UnityLogFilter), nameof(ChainloaderHook))));
         }
 
         public static void ChainloaderHook()
         {
-            harmony.Patch(AccessTools.Method(typeof(UnityLogSource), "UnityLogMessageHandler"),
+            Harmony.Patch(AccessTools.Method(typeof(UnityLogSource), "UnityLogMessageHandler"),
                           prefix: new HarmonyMethod(AccessTools.Method(typeof(UnityLogFilter), nameof(LogPatch))));
         }
 
