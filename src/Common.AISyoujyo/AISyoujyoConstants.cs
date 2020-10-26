83724a5bdc9d136ba20c58986cf7d9a7623e5bc5
Mantas-2155X <syenergyy@gmail.com>
2020-10-26 13:45:05 +0200

    * Fix not working properly in AI * Added process filter for Steam version

--- src/Common.AISyoujyo/AISyoujyoConstants.cs
+++ src/Common.AISyoujyo/AISyoujyoConstants.cs
@@ -3,6 +3,7 @@
     internal static class AISyoujyoConstants
     {
         public const string MainGameProcessName = "AI-Syoujyo";
+        public const string SteamGameProcessName = "AI-Shoujo";
         public const string StudioProcessName = "StudioNEOV2";
     }
 }
