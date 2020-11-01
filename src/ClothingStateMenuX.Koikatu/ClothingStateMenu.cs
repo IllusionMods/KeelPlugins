352dfab1520f8a48ba11f077fa56d26ee715a982
Keelhauled <keelhauledhs@gmail.com>
2020-11-02 01:37:12 +0200

    ClothingStateMenuX

---
+++ src/ClothingStateMenuX.Koikatu/ClothingStateMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+
+namespace ClothingStateMenuX.Koikatu
+{
+    [BepInProcess(KeelPlugins.KoikatuConstants.MainGameProcessName)]
+    [BepInProcess(KeelPlugins.KoikatuConstants.MainGameProcessNameSteam)]
+    [BepInPlugin("keelhauled.clothingstatemenux", "ClothingStateMenuX", Version)]
+    public class ClothingStateMenu : BaseUnityPlugin
+    {
+        public const string Version = "1.0.0." + BuildNumber.Version;
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(GetType());
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(CustomScene), "Start")]
+        private static void MakerEntrypoint()
+        {
+            UI.CreateTitle("Clothing Sets", 0);
+            UI.CreateClothingSets(1);
+            UI.CreateSeparator(2);
+
+            UI.CreateClothingOptions(VanillaUI.ClothingStateToggles.transform.GetSiblingIndex() + 1);
+            UI.CreateAccessories(VanillaUI.AccessoryToggles.transform.GetSiblingIndex() + 1);
+        }
+    }
+}
---
+++ src/Testing.Koikatu/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UILib;
+using UnityEngine.Events;
+using HarmonyLib;
+using ChaCustom;
+
+namespace Testing.Koikatu
+{
+    [BepInPlugin("keelhauled.testing", "TESTINGPLUGIN", "1.0.0")]
+    public class Testing : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+    }
+}
