5e2d7f6d045ed21952facdaa6bf8d2329b0ec20a
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-11-02 21:05:08 +0200

    Improve fonts

--- src/ClothingStateMenuX.Koikatu/UI.cs
+++ src/ClothingStateMenuX.Koikatu/UI.cs
@@ -15,15 +15,15 @@ namespace ClothingStateMenuX.Koikatu
     {
         public static GameObject CreateClothingSets(int index)
         {
-            var container = CreateContainer(27, index);
+            var container = CreateContainer(25, index);
 
-            var btn1 = CreateButton("1", () => VanillaUI.OutfitDropDown.value = 0, container.transform);
-            var btn2 = CreateButton("2", () => VanillaUI.OutfitDropDown.value = 1, container.transform);
-            var btn3 = CreateButton("3", () => VanillaUI.OutfitDropDown.value = 2, container.transform);
-            var btn4 = CreateButton("4", () => VanillaUI.OutfitDropDown.value = 3, container.transform);
-            var btn5 = CreateButton("5", () => VanillaUI.OutfitDropDown.value = 4, container.transform);
-            var btn6 = CreateButton("6", () => VanillaUI.OutfitDropDown.value = 5, container.transform);
-            var btn7 = CreateButton("7", () => VanillaUI.OutfitDropDown.value = 6, container.transform);
+            var btn1 = CreateButton("1", 14, () => VanillaUI.OutfitDropDown.value = 0, container.transform);
+            var btn2 = CreateButton("2", 14, () => VanillaUI.OutfitDropDown.value = 1, container.transform);
+            var btn3 = CreateButton("3", 14, () => VanillaUI.OutfitDropDown.value = 2, container.transform);
+            var btn4 = CreateButton("4", 14, () => VanillaUI.OutfitDropDown.value = 3, container.transform);
+            var btn5 = CreateButton("5", 14, () => VanillaUI.OutfitDropDown.value = 4, container.transform);
+            var btn6 = CreateButton("6", 14, () => VanillaUI.OutfitDropDown.value = 5, container.transform);
+            var btn7 = CreateButton("7", 14, () => VanillaUI.OutfitDropDown.value = 6, container.transform);
 
             var pos = 0.03f;
             var step = (1f - pos * 2) / 7f;
@@ -44,38 +44,41 @@ namespace ClothingStateMenuX.Koikatu
 
             var delete = new List<GameObject>
             {
-                CreateClothingStateButtons("Top", ChaFileDefine.ClothesKind.top),
-                CreateClothingStateButtons("Bottom", ChaFileDefine.ClothesKind.shorts),
-                CreateClothingStateButtons("Bra", ChaFileDefine.ClothesKind.bra),
-                CreateClothingStateButtons("Underwear", ChaFileDefine.ClothesKind.bot),
-                CreateClothingStateButtons("Pantyhose", ChaFileDefine.ClothesKind.panst),
-                CreateClothingStateButtons("Gloves", ChaFileDefine.ClothesKind.gloves),
-                CreateClothingStateButtons("Legwear", ChaFileDefine.ClothesKind.socks),
-                CreateClothingStateButtons("Shoes", ChaFileDefine.ClothesKind.shoes_inner)
+                CreateClothingStateButtons("Top", ChaFileDefine.ClothesKind.top, 3),
+                CreateClothingStateButtons("Bottom", ChaFileDefine.ClothesKind.bot, 3),
+                CreateClothingStateButtons("Bra", ChaFileDefine.ClothesKind.bra, 3),
+                CreateClothingStateButtons("Underwear", ChaFileDefine.ClothesKind.shorts, 4),
+                CreateClothingStateButtons("Pantyhose", ChaFileDefine.ClothesKind.panst, 3),
+                CreateClothingStateButtons("Gloves", ChaFileDefine.ClothesKind.gloves, 2),
+                CreateClothingStateButtons("Legwear", ChaFileDefine.ClothesKind.socks, 2),
+                CreateClothingStateButtons("Shoes", ChaFileDefine.ClothesKind.shoes_inner, 2)
             };
 
             return delete;
 
-            GameObject CreateClothingStateButtons(string text, ChaFileDefine.ClothesKind kind)
+            GameObject CreateClothingStateButtons(string text, ChaFileDefine.ClothesKind kind, int buttons)
             {
-                var container = CreateContainer(25, index + counter++);
+                var container = CreateContainer(22, index + counter++);
 
+                var margin = 0.03f;
                 var pos = 0.4f;
-                var step = (1f - pos - 0.03f) / 4f;
+                var step = (1f - pos - margin) / 4f;
 
-                var textElem = CreateText(text, container.transform);
-                textElem.transform.SetRect(0.03f, 0f, pos, 1f);
+                var textElem = CreateText(text, 12, container.transform);
+                textElem.transform.SetRect(margin, 0f, pos, 1f);
 
-                var buttonOn = CreateButton("On", () => VanillaUI.Character.SetClothesState((int)kind, 0), container.transform);
+                var buttonOn = CreateButton("On", 10, () => VanillaUI.Character.SetClothesState((int)kind, 0), container.transform);
                 buttonOn.transform.SetRect(pos, 0f, pos += step, 1f);
 
-                var buttonHalf1 = CreateButton("½", () => VanillaUI.Character.SetClothesState((int)kind, 1), container.transform);
+                var buttonHalf1 = CreateButton("½", 10, () => VanillaUI.Character.SetClothesState((int)kind, 1), container.transform);
                 buttonHalf1.transform.SetRect(pos, 0f, pos += step, 1f);
+                if(buttons < 3) buttonHalf1.GetComponent<Button>().interactable = false;
 
-                var buttonHalf2 = CreateButton("½", () => VanillaUI.Character.SetClothesState((int)kind, 2), container.transform);
+                var buttonHalf2 = CreateButton("½", 10, () => VanillaUI.Character.SetClothesState((int)kind, 2), container.transform);
                 buttonHalf2.transform.SetRect(pos, 0f, pos += step, 1f);
+                if(buttons < 4) buttonHalf2.GetComponent<Button>().interactable = false;
 
-                var buttonOff = CreateButton("Off", () => VanillaUI.Character.SetClothesState((int)kind, 3), container.transform);
+                var buttonOff = CreateButton("Off", 10, () => VanillaUI.Character.SetClothesState((int)kind, 3), container.transform);
                 buttonOff.transform.SetRect(pos, 0f, pos += step, 1f);
 
                 return container;
@@ -90,14 +93,14 @@ namespace ClothingStateMenuX.Koikatu
             return copyTxt;
         }
 
-        public static GameObject CreateText(string text, Transform parent)
+        public static GameObject CreateText(string text, float fontSize, Transform parent)
         {
             var copyTxt = GameObject.Instantiate(VanillaUI.NormalTextTemplate, parent);
 
             var textMesh = copyTxt.GetComponentInChildren<TextMeshProUGUI>();
             textMesh.text = text;
             textMesh.enableAutoSizing = false;
-            textMesh.fontSize = 12;
+            textMesh.fontSize = fontSize;
             textMesh.margin = new Vector4(0, 0);
             textMesh.transform.SetRect();
             textMesh.alignment = TextAlignmentOptions.Center;
@@ -112,14 +115,14 @@ namespace ClothingStateMenuX.Koikatu
             return sep;
         }
 
-        public static GameObject CreateButton(string text, UnityAction onClick, Transform parent)
+        public static GameObject CreateButton(string text, float fontSize, UnityAction onClick, Transform parent)
         {
             var copyBtn = GameObject.Instantiate(VanillaUI.ButtonTemplate, parent);
 
             var textMesh = copyBtn.GetComponentInChildren<TextMeshProUGUI>();
             textMesh.text = text;
             textMesh.enableAutoSizing = false;
-            textMesh.fontSize = 10;
+            textMesh.fontSize = fontSize;
             textMesh.margin = new Vector4(0, 0);
             textMesh.transform.SetRect();
 
@@ -140,6 +143,15 @@ namespace ClothingStateMenuX.Koikatu
                 GameObject.DestroyImmediate(t.gameObject);
 
             return copy;
+        }
+
+        public static void SetRect(this Transform self, float anchorLeft = 0f, float anchorBottom = 0f, float anchorRight = 1f, float anchorTop = 1f, float offsetLeft = 0f, float offsetBottom = 0f, float offsetRight = 0f, float offsetTop = 0f)
+        {
+            RectTransform rt = self as RectTransform;
+            rt.anchorMin = new Vector2(anchorLeft, anchorBottom);
+            rt.anchorMax = new Vector2(anchorRight, anchorTop);
+            rt.offsetMin = new Vector2(offsetLeft, offsetBottom);
+            rt.offsetMax = new Vector2(offsetRight, offsetTop);
         }
     }
 }
