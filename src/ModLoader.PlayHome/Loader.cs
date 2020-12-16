09997b6e27c3de747bc492db35da5897cd1ca533
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-12-16 16:35:20 +0200

    Don't create temp file

--- src/ModLoader.PlayHome/Loader.cs
+++ src/ModLoader.PlayHome/Loader.cs
@@ -14,7 +14,7 @@ namespace ModLoader.PlayHome
         public const string GUID = "keelhauled.modloader";
         public const string Version = "1.0.0." + BuildNumber.Version;
 
-        internal static string ModFolder = Path.Combine(Paths.GameRootPath, "mods");
+        private static string ModFolder = Path.Combine(Paths.GameRootPath, "mods");
         private static Dictionary<string, string> AssetAbdata = new Dictionary<string, string>();
 
         private void Awake()
@@ -33,12 +33,12 @@ namespace ModLoader.PlayHome
 
             {
                 var targetMethod = AccessTools.Method(typeof(AssetBundleController), nameof(AssetBundleController.LoadAsset)).MakeGenericMethod(typeof(UnityEngine.Object));
-                var patchMethod = AccessTools.Method(typeof(Loader), nameof(LoadAsset));
+                var patchMethod = AccessTools.Method(typeof(Loader), nameof(LoadThumbs));
                 harmony.Patch(targetMethod, new HarmonyMethod(patchMethod));
             }
         }
 
-        public static bool LoadAsset(ref object __result, ref string assetName, AssetBundleController __instance)
+        private static bool LoadThumbs(ref object __result, ref string assetName, AssetBundleController __instance)
         {
             if(__instance.assetBundleName.Contains("thumnbnail/thumbnail_") || __instance.assetBundleName.Contains("thumnbnail/thumnbs_"))
             {
@@ -54,13 +54,6 @@ namespace ModLoader.PlayHome
             }
 
             return true;
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(ItemDataBase), MethodType.Constructor, new[] { typeof(int), typeof(string), typeof(string), typeof(int), typeof(bool) })]
-        private static void ExtendIds(ref int ___id, int id)
-        {
-            ___id = id > 999999 && id < 1000000000 ? id : id % 1000;
         }
 
         private static Texture2D LoadPNG(string file)
@@ -83,6 +76,13 @@ namespace ModLoader.PlayHome
             }
         }
 
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ItemDataBase), MethodType.Constructor, new[] { typeof(int), typeof(string), typeof(string), typeof(int), typeof(bool) })]
+        private static void ExtendIds(ref int ___id, int id)
+        {
+            ___id = id > 999999 && id < 1000000000 ? id : id % 1000;
+        }
+
         private static void LoadMods(Dictionary<int, object> datas, ref string search, Action<Dictionary<int, object>, AssetBundleController, CustomDataListLoader> ___action)
         {
             var dir = "";
@@ -97,91 +97,40 @@ namespace ModLoader.PlayHome
 
             foreach(var folder in Directory.GetDirectories(ModFolder))
             {
-                var modAbDir = Path.Combine(folder, "abdata");
-                var modListDir = BepInEx.Utility.CombinePaths(folder, "abdata", "list", dir);
+                var abdataDir = Path.Combine(folder, "abdata");
+                var listDir = BepInEx.Utility.CombinePaths(abdataDir, "list", dir);
 
-                if(Directory.Exists(modListDir))
+                if(Directory.Exists(listDir))
                 {
-                    foreach(var list in Directory.GetFiles(modListDir, filter + "_Mlist.txt"))
+                    foreach(var listPath in Directory.GetFiles(listDir, filter + "_Mlist.txt"))
                     {
-                        using(var fileStream = new FileStream(list, FileMode.Open))
+                        using(var fileStream = new FileStream(listPath, FileMode.Open))
                         using(var streamReader = new StreamReader(fileStream))
                         {
                             var assetBundleName = streamReader.ReadLine();
-                            var contents = streamReader.ReadToEnd();
 
-                            var tempFileName = Path.GetTempFileName();
-                            File.WriteAllText(tempFileName, contents);
-                            var customDataListLoader = new CustomDataListLoader();
-                            customDataListLoader.Load(tempFileName);
-                            File.Delete(tempFileName);
+                            var listLoader = new CustomDataListLoader();
+                            Traverse.Create(listLoader).Method("Load", new[] { typeof(TextReader) }).GetValue(streamReader);
 
-                            var assetBundleController2 = new AssetBundleController();
-                            assetBundleController2.OpenFromFile(Path.Combine(folder, "abdata"), assetBundleName);
+                            var abc = new AssetBundleController();
+                            abc.OpenFromFile(Path.Combine(folder, "abdata"), assetBundleName);
 
                             var count = datas.Count;
-                            ___action(datas, assetBundleController2, customDataListLoader);
+                            ___action(datas, abc, listLoader);
                             for(int i = count; i < datas.Count; i++)
                             {
                                 var prefabData = (PrefabData)datas.Values.ElementAt(i);
-                                prefabData.assetbundleDir = modAbDir;
+                                prefabData.assetbundleDir = abdataDir;
                             }
 
-                            assetBundleController2.Close(false);
+                            abc.Close(false);
                         }
                     }
 
-                    foreach(var item in Directory.GetFiles(Path.Combine(modAbDir, "thumnbnail_R"), "*.png"))
-                        AssetAbdata[Path.GetFileNameWithoutExtension(item)] = modAbDir;
+                    foreach(var thumbPath in Directory.GetFiles(Path.Combine(abdataDir, "thumnbnail_R"), "*.png"))
+                        AssetAbdata[Path.GetFileNameWithoutExtension(thumbPath)] = abdataDir;
                 }
             }
         }
-
-        //[HarmonyPrefix]
-        //[HarmonyPatch(typeof(EditMode), "CreateData", new[] { typeof(PrefabData), typeof(Rect), typeof(AssetBundleController) })]
-        //[HarmonyPatch(typeof(EditMode), "CreateData", new[] { typeof(CombineTextureData), typeof(Rect), typeof(AssetBundleController) })]
-        //[HarmonyPatch(typeof(EditMode), "CreateData", new[] { typeof(HeadData), typeof(Rect), typeof(AssetBundleController) })]
-        //[HarmonyPatch(typeof(EditMode), "CreateData", new[] { typeof(AccessoryData), typeof(Rect), typeof(AssetBundleController) })]
-        //private static bool LoadThumbs(ref CustomSelectSet __result, Rect smallRect, AssetBundleController abc, ItemDataBase item)
-        //{
-        //    if(item.id == 721523105)
-        //    {
-        //        Log.Info("TEMP");
-        //    }
-
-        //    var traverse = Traverse.Create(item);
-        //    var prefab = traverse.Field("prefab").GetValue<string>();
-        //    var textureName = traverse.Field("textureName").GetValue<string>();
-        //    var path = traverse.Field("path").GetValue<string>();
-        //    var prefab_F = traverse.Field("prefab_F").GetValue<string>();
-        //    var assetName = prefab ?? textureName ?? path ?? prefab_F;
-
-        //    Texture2D texture = null;
-        //    if(item.assetbundleDir.Contains(ModFolder))
-        //    {
-        //        if(abc.assetBundleName.Contains("thumnbnail/thumbnail_") || abc.assetBundleName.Contains("thumnbnail/thumnbs_"))
-        //        {
-        //            string thumbPath = BepInEx.Utility.CombinePaths(item.assetbundleDir, "thumnbnail_R", assetName + ".png");
-        //            if(File.Exists(thumbPath))
-        //            {
-        //                Log.Info("Load thumb: " + thumbPath);
-        //                texture = LoadPNG(thumbPath);
-        //            }
-        //            else
-        //            {
-        //                Log.Info($"Thumbnail '{thumbPath}' does not exist");
-        //            }
-        //        }
-        //    }
-        //    else
-        //        texture = abc.LoadAsset<Texture2D>(assetName);
-
-        //    var vector = new Vector2(256f, 256f);
-        //    var thumbnail_L = Sprite.Create(texture, new Rect(Vector2.zero, vector), vector * 0.5f, 100f, 0U, SpriteMeshType.FullRect);
-        //    var thumbnail_S = Sprite.Create(texture, smallRect, smallRect.size * 0.5f, 100f, 0U, SpriteMeshType.FullRect);
-        //    __result = new CustomSelectSet(item.id, item.name_LineFeed, thumbnail_S, thumbnail_L, item.isNew);
-
-        //    return false;
-        //}
     }
 }
