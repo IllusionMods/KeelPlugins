9665246a5e8333535293ab5fc8f7b4e079e2eaf9
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-11-09 19:58:35 +0100

    TitleShortcuts.AISyoujyo - Add "Open main game" hotkey

--- src/TitleShortcuts.AISyoujyo/TitleShortcuts.cs
+++ src/TitleShortcuts.AISyoujyo/TitleShortcuts.cs
@@ -17,12 +17,13 @@ namespace TitleShortcuts.AISyoujyo
     [BepInPlugin(GUID, PluginName, Version)]
     public class TitleShortcuts : TitleShortcutsCore
     {
-        public const string Version = "1.2.2." + BuildNumber.Version;
+        public const string Version = "1.3.0." + BuildNumber.Version;
 
         private ConfigEntry<KeyboardShortcut> StartFemaleMaker { get; set; }
         private ConfigEntry<KeyboardShortcut> StartMaleMaker { get; set; }
         private ConfigEntry<KeyboardShortcut> StartUploader { get; set; }
         private ConfigEntry<KeyboardShortcut> StartDownloader { get; set; }
+        private ConfigEntry<KeyboardShortcut> StartGame { get; set; }
 
         private TitleScene titleScene;
 
@@ -34,6 +35,7 @@ namespace TitleShortcuts.AISyoujyo
             StartMaleMaker = Config.Bind(SECTION_HOTKEYS, "Open male maker", new KeyboardShortcut(KeyCode.M));
             StartUploader = Config.Bind(SECTION_HOTKEYS, "Open uploader", new KeyboardShortcut(KeyCode.U));
             StartDownloader = Config.Bind(SECTION_HOTKEYS, "Open downloader", new KeyboardShortcut(KeyCode.D));
+            StartGame = Config.Bind(SECTION_HOTKEYS, "Open main game", new KeyboardShortcut(KeyCode.G));
 
             SceneManager.sceneLoaded += StartInput;
         }
@@ -57,6 +59,7 @@ namespace TitleShortcuts.AISyoujyo
                     else if (StartMaleMaker.Value.IsPressed()) StartMode(titleScene.OnCustomMale, "Starting male maker");
                     else if (StartUploader.Value.IsPressed()) StartMode(titleScene.OnUploader, "Starting uploader");
                     else if (StartDownloader.Value.IsPressed()) StartMode(titleScene.OnDownloader, "Starting downloader");
+                    else if (StartGame.Value.IsPressed()) StartMode(titleScene.OnStart, "Starting main game");
                 }
 
                 yield return null;
