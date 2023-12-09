b25efd0d580063467ea42a7e45e9fddddaa2f00f
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2023-12-09 22:30:32 +0200

    TesselationSetting plugin (WIP)

---
+++ src/TesselationSetting.KoikatuSunshine/CharaExtra.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using KeelPlugins.Koikatu;
+using KKAPI;
+using KKAPI.Chara;
+using KKAPI.Maker;
+using UnityEngine;
+
+namespace TesselationSetting.Koikatu
+{
+    public class CharaExtra : CharaCustomFunctionController
+    {
+        protected override void OnCardBeingSaved(GameMode currentGameMode)
+        {
+            
+        }
+
+        protected override void OnReload(GameMode currentGameMode)
+        {
+            StartCoroutine(Coroutine());
+
+            IEnumerator Coroutine()
+            {
+                for(int i = 0; i < 10; i++)
+                    yield return null;
+                
+                SetRendererTesselation(ChaControl.rendBody);
+                SetRendererTesselation(ChaControl.rendFace);
+            }
+        }
+
+        private static void SetRendererTesselation(Renderer rend)
+        {
+            if(rend.material && rend.material.shader.name == "xukmi/SkinPlusTess")
+            {
+                if(rend.material.HasProperty("_TessSmooth"))
+                {
+                    Log.Info($"Set TessSmooth {TesselationSetting.TessSmooth.Value} '{rend.material.name}'");
+                    rend.material.SetFloat("_TessSmooth", TesselationSetting.TessSmooth.Value);
+                }
+            }
+        }
+    }
+}
---
+++ src/TesselationSetting.KoikatuSunshine/TesselationSetting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using KeelPlugins.Koikatu;
+using KKAPI;
+using KKAPI.Maker;
+using UnityEngine;
+
+[assembly: System.Reflection.AssemblyFileVersion(TesselationSetting.Koikatu.TesselationSetting.Version)]
+
+namespace TesselationSetting.Koikatu
+{
+    [BepInProcess(Constants.MainGameProcessName)]
+    [BepInDependency(KoikatuAPI.GUID)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class TesselationSetting : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.tesselationsetting";
+        public const string PluginName = "TesselationSetting";
+        public const string Version = "1.0.0." + BuildNumber.Version;
+
+        public static ConfigEntry<float> TessSmooth { get; set; }
+
+        private static TesselationSetting plugin;
+        private static readonly List<Renderer> ManagedRenderers = new List<Renderer>();
+
+        private void Awake()
+        {
+            plugin = this;
+            Log.SetLogSource(Logger);
+
+            TessSmooth = Config.Bind("General", "TessSmooth", 0f, new ConfigDescription("", new AcceptableValueRange<float>(0f, 1f)));
+
+            KKAPI.Chara.CharacterApi.RegisterExtraBehaviour<CharaExtra>(GUID);
+        }
+    }
+}
