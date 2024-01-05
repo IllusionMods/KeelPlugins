3762a57ff6adc1a195e516ad86ca20f7f1e47dc3
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2024-01-05 03:30:43 +0200

    [EyeLookEditor] No decimals in sliders

--- src/EyeLookEditor.KoikatuSunshine/EyeLookEditor.cs
+++ src/EyeLookEditor.KoikatuSunshine/EyeLookEditor.cs
@@ -31,18 +31,30 @@ namespace EyeLookEditor
             var stringToValue = new Func<string, float>(float.Parse);
             var valueToString = new Func<float, string>(f => f.ToString("0.#"));
 
-            var slider_thresholdAngleDifference = e.AddControl(new MakerSlider(category, "thresholdAngleDifference", -360f, 360f, DefaultValue.ThresholdAngleDifference, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_bendingMultiplier = e.AddControl(new MakerSlider(category, "bendingMultiplier", -100f, 100f, DefaultValue.BendingMultiplier, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_maxAngleDifference = e.AddControl(new MakerSlider(category, "maxAngleDifference", -360f, 360f, DefaultValue.MaxAngleDifference, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_upBendingAngle = e.AddControl(new MakerSlider(category, "upBendingAngle", -360f, 360f, DefaultValue.UpBendingAngle, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_downBendingAngle = e.AddControl(new MakerSlider(category, "downBendingAngle", -360f, 360f, DefaultValue.DownBendingAngle, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_minBendingAngle = e.AddControl(new MakerSlider(category, "minBendingAngle", -360f, 360f, DefaultValue.MinBendingAngle, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_maxBendingAngle = e.AddControl(new MakerSlider(category, "maxBendingAngle", -360f, 360f, DefaultValue.MaxBendingAngle, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_leapSpeed = e.AddControl(new MakerSlider(category, "Tracking Speed", 0f, 50f, DefaultValue.LeapSpeed, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_forntTagDis = e.AddControl(new MakerSlider(category, "forntTagDis", -100f, 100f, DefaultValue.ForntTagDis, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_nearDis = e.AddControl(new MakerSlider(category, "nearDis", -100f, 100f, DefaultValue.NearDis, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_hAngleLimit = e.AddControl(new MakerSlider(category, "Horizontal Tracking Limit", 0f, 360f, DefaultValue.HAngleLimit, this) { StringToValue = stringToValue, ValueToString = valueToString });
-            var slider_vAngleLimit = e.AddControl(new MakerSlider(category, "Vertical Tracking Limit", 0f, 360f, DefaultValue.VAngleLimit, this) { StringToValue = stringToValue, ValueToString = valueToString });
+            var slider_thresholdAngleDifference = e.AddControl(new MakerSlider(category, "thresholdAngleDifference", -360f, 360f, DefaultValue.ThresholdAngleDifference, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_bendingMultiplier = e.AddControl(new MakerSlider(category, "bendingMultiplier", -100f, 100f, DefaultValue.BendingMultiplier, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_maxAngleDifference = e.AddControl(new MakerSlider(category, "maxAngleDifference", -360f, 360f, DefaultValue.MaxAngleDifference, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_upBendingAngle = e.AddControl(new MakerSlider(category, "upBendingAngle", -360f, 360f, DefaultValue.UpBendingAngle, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_downBendingAngle = e.AddControl(new MakerSlider(category, "downBendingAngle", -360f, 360f, DefaultValue.DownBendingAngle, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_minBendingAngle = e.AddControl(new MakerSlider(category, "minBendingAngle", -360f, 360f, DefaultValue.MinBendingAngle, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_maxBendingAngle = e.AddControl(new MakerSlider(category, "maxBendingAngle", -360f, 360f, DefaultValue.MaxBendingAngle, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_leapSpeed = e.AddControl(new MakerSlider(category, "Tracking Speed", 0f, 50f, DefaultValue.LeapSpeed, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_forntTagDis = e.AddControl(new MakerSlider(category, "forntTagDis", -100f, 100f, DefaultValue.ForntTagDis, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_nearDis = e.AddControl(new MakerSlider(category, "nearDis", -100f, 100f, DefaultValue.NearDis, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_hAngleLimit = e.AddControl(new MakerSlider(category, "Horizontal Tracking Limit", 0f, 360f, DefaultValue.HAngleLimit, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
+            var slider_vAngleLimit = e.AddControl(new MakerSlider(category, "Vertical Tracking Limit", 0f, 360f, DefaultValue.VAngleLimit, this)
+                { StringToValue = stringToValue, ValueToString = valueToString, WholeNumbers = true });
 
             slider_thresholdAngleDifference.BindToFunctionController<EyeLookCharaController, float>(ctrl => ctrl.ThresholdAngleDifference, (ctrl, f) => ctrl.ThresholdAngleDifference = f);
             slider_bendingMultiplier.BindToFunctionController<EyeLookCharaController, float>(ctrl => ctrl.BendingMultiplier, (ctrl, f) => ctrl.BendingMultiplier = f);
